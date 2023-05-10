31dc78ac1f7032dbcbb519a58e3937f73ad42cd0
jjaenim <joshuatead@gmail.com>
2023-05-10 23:11:47 +0800

    Pseudocode

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,12 @@
 # Joshua Adrian O. Daet
 # BSCpE 1-4
 # Python Caluclator
+
+# Pseudocode
+# Operation Methods
+# Asks the user to choose what math operation will be used (Addition, Subtraction, Multiplication and Division)
+# Asks the user for the two numbers that'll be used for the equation chosen
+# Displays the result
+# Asks the user if the users wants to try again or not
+    #If yes, repeat Step 1
+    #If no, display "Thank you"
